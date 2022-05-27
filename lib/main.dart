bd0010d6dc72bec928bad889cac360dbe049fbda
EmmanuelPereira1 <97740932+EmmanuelPereira1@users.noreply.github.com>
2022-05-27 12:20:48 -0300

    built_ios_app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:convert_coin/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'app_widget.dart';
 
 void main() async {
    WidgetsFlutterBinding.ensureInitialized();
-   await Firebase.initializeApp();
+   await Firebase.initializeApp(
+     options: DefaultFirebaseOptions.ios,
+   );
   runApp(const AppWidget());
 }
 
